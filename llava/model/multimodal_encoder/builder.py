c3b57066b7f0e142b314b9fb089901eaaf874d14
jymChen <junying.chen.cs@gmail.com>
2024-07-24 12:55:24 +0800

    update

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -8,6 +8,8 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     if not is_absolute_path_exists and vision_tower:
         vision_tower = os.path.join(getattr(vision_tower_cfg, '_name_or_path', None),vision_tower)
         is_absolute_path_exists = os.path.exists(vision_tower)
-    if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion"):
+    if is_absolute_path_exists or vision_tower.startswith("openai"):
         return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    else:
+    	return CLIPVisionTower('openai/clip-vit-large-patch14-336', args=vision_tower_cfg, **kwargs)
     raise ValueError(f'Unknown vision tower: {vision_tower}')
